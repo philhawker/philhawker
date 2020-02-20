de66cdc2c7af643f40023349b92a776c0a3f6628
Phil Hawker <philhawker1211@gmail.com>
2020-02-20 16:28:41 -0700

    added class compenent

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div className='PortfolioConatiner'>
+                <h2>portfolio conatiner goes here</h2>
+            </div>
+        );
+    }
+}
