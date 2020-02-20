b90d82d0224c2b81c9154afd8f381c690945d052
Phil Hawker <philhawker1211@gmail.com>
2020-02-20 10:17:14 -0700

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>Phil Hawker's Portfolio</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='app'>
+                <h1>Phil Hawker's Portfolio</h1>
+            </div>
+        );
+    }
 }
