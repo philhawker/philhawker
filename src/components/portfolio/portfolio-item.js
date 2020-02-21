0c665d4de0080d2c22c94e3233708bee66299ba1
Phil Hawker <philhawker1211@gmail.com>
2020-02-21 11:27:01 -0700

    added props

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default function portfolioItems(props) {
     return (
         <div>
-            <h3>{ props.title } </h3>
-            <h4>{ props.url } </h4>
+            <h3>{props.title} </h3>
+            <h4>{props.url} </h4>
         </div>
     );
 }
