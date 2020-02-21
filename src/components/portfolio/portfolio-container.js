d0546bbd65316e72a16b2d7ef7d3932d9822b99f
Phil Hawker <philhawker1211@gmail.com>
2020-02-21 09:54:26 -0700

    created portfolio item

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import PortfolioItem from './portfolio-item';
+import portfolioItem from './portfolio-item';
+
 export default class PortfolioContainer extends Component {
     render() {
         return (
             <div className='PortfolioConatiner'>
                 <h2>portfolio conatiner goes here</h2>
+
+                <PortfolioItem />
             </div>
         );
     }
