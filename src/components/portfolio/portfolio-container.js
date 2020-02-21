977a1e470fc8ecbf5c37153c864e757078b09f4e
Phil Hawker <philhawker1211@gmail.com>
2020-02-21 10:08:27 -0700

    edited functional component

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import PortfolioItem from './portfolio-item';
-import portfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
     render() {
