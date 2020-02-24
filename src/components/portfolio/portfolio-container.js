a0c14bb41a1609626c2c3e3de5890a72f797b9c6
Phil Hawker <philhawker1211@gmail.com>
2020-02-24 14:42:06 -0700

    added isLoading

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,6 +8,7 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: 'Welcome to my portfolio',
+            isLoading: false,
             data: [
                 { title: 'Vivint', category: 'eCommerce' },
                 { title: 'Vox', category: 'Scheduling' },
@@ -35,14 +36,23 @@ export default class PortfolioContainer extends Component {
 
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
         return (
             <div>
 
                 <h2>{this.state.pageTitle}</h2>
 
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+                <button onClick={() => this.handleFilter('eCommerce')}>
+                    eCommerce
+                </button>
+                <button onClick={() => this.handleFilter('Scheduling')}>
+                    Scheduling
+                </button>
+                <button onClick={() => this.handleFilter('Enterprise')}>
+                    Enterprise
+                </button>
 
                 {this.portfolioItems()}
 
