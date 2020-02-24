815cae4ac0e1f048d1176e3c12dd8508365da572
Phil Hawker <philhawker1211@gmail.com>
2020-02-24 15:47:19 -0700

    added about and home route

--- src/components/app.js
+++ src/components/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
                         <NavigationContainer />
                         <Switch>
                             <Route exact path='/' component={Home} />
-                            <Route exact path='/about-me' component={About} />
+                            <Route path='/about-me' component={About} />
                         </Switch>
                     </div>
                 </Router>
