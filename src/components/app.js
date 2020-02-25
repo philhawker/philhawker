460459581c089b1fcd337665b851e58b9d6dbf9c
Phil Hawker <philhawker1211@gmail.com>
2020-02-25 10:25:59 -0700

    added exact path rule for slug route so added invalide url routes arew denied

--- src/components/app.js
+++ src/components/app.js
@@ -26,7 +26,7 @@ export default class App extends Component {
                             <Route path='/about-me' component={About} />
                             <Route path='/contact' component={Contact} />
                             <Route path='/blog' component={Blog} />
-                            <Route path='/portfolio/:slug' component={PortfolioDetail} />
+                            <Route exact path='/portfolio/:slug' component={PortfolioDetail} />
                             <Route component={NoMatch} />
                         </Switch>
                     </div>
