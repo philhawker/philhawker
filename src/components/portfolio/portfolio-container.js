4ee5bb335a12ea564b47aa08a7b2bb4f9acef458
Phil Hawker <philhawker1211@gmail.com>
2020-02-25 15:33:05 -0700

    changed what data is pulled from the devcamp space api

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,16 +10,10 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: 'Welcome to my portfolio',
             isLoading: false,
-            data: [
-                { title: 'Vivint', category: 'eCommerce', slug: 'vivint' },
-                { title: 'Vox', category: 'Scheduling', slug: 'vox' },
-                { title: 'Grove Creek Foods', category: 'Enterprise', slug: 'grove-creek-foods' },
-                { title: 'Bottega', category: 'eCommerce', slug: 'bottega' }
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this)
-        this.getPortfolioItems = this.getPortfolioItems.bind()
     }
 
     handleFilter(filter) {
@@ -34,6 +28,9 @@ export default class PortfolioContainer extends Component {
         axios.get('https://philhawker.devcamp.space/portfolio/portfolio_items')
             .then(response => {
                 console.log('Response Data', response);
+                this.setState({
+                    data: response.data.portfolio_items
+                })
             })
             .catch(error => {
                 console.log(error);
@@ -42,13 +39,16 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
         return this.state.data.map(item => {
+            console.log('item data', item)
             return <PortfolioItem title={item.title} url={'Google.com'} slug={item.slug} />;
         });
     }
 
+    componentDidMount() {
+        this.getPortfolioItems()
+    }
 
     render() {
-        this.getPortfolioItems()
         if (this.state.isLoading) {
             return <div>Loading...</div>
         }
