aa034ff22ad9c22aef94851b17b427161a723605
Phil Hawker <philhawker1211@gmail.com>
2020-02-26 16:34:22 -0700

    added auth background image. created return classes

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import loginImg from '../../../static/assets/images/auth/login.jpg'
 
 export default class Auth extends Component {
     constructor() {
@@ -7,9 +8,18 @@ export default class Auth extends Component {
 
     render() {
         return (
-            <div>
-                Auth...
-        </div>
+            <div className='auth-page-wrapper'>
+                <div
+                    className='left-column'
+                    style={{
+                        backgroundImage: `url(${loginImg})`
+                    }}
+                />
+
+                <div className='right-column'>
+                    <h1>Login component goes here...</h1>
+                </div>
+            </div>
         )
     }
 }
