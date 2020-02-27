fd81d164c9eb1e5c7a7d6f372ee88859769e66a6
Phil Hawker <philhawker1211@gmail.com>
2020-02-27 10:56:28 -0700

    created login form fields

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Login from '../auth/login'
 import loginImg from '../../../static/assets/images/auth/login.jpg'
 
 export default class Auth extends Component {
@@ -17,7 +18,7 @@ export default class Auth extends Component {
                 />
 
                 <div className='right-column'>
-                    <h1>Login component goes here...</h1>
+                <Login />
                 </div>
             </div>
         )
