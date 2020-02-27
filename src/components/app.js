6018562b7dcb1b1b3ac8036bbdb74858199b6ea9
Phil Hawker <philhawker1211@gmail.com>
2020-02-27 13:46:28 -0700

    created logged in handlers and conditions to check login status

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home'
@@ -33,6 +34,34 @@ export default class App extends Component {
         this.setState({
             loggedInStatus: 'NOT_LOGGED_IN'
         })
+    }
+
+    checkLoginStatus () {
+        return axios.get('https://api.devcamp.space/logged_in', {
+            withCredentials: true // axios returns a promise. with any promise, 'then' needs to be called
+        }).then(response => {
+            const loggedIn = response.data.logged_in
+            const loggedInStatus = this.state.loggedInStatus
+
+            if (loggedIn && loggedInStatus === 'LOGGED_IN') {
+                return loggedIn
+            } else if (loggedIn && loggedInStatus === 'NOT_LOGGED_IN') {
+                this.setState({
+                    loggedInStatus: 'LOGGED_IN'
+                })
+            } else if (!loggedIn && loggedInStatus === 'LOGGED_IN') {
+                this.setState({
+                    loggedInStatus: 'NOT_LOGGED_IN'
+                })
+            }
+        })
+        .catch(error => {
+            console.log('Error', error)
+        })
+    }
+
+    componentDidMount() {
+        this.checkLoginStatus()
     }
 
     render() {
