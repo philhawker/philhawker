aadd1ac7a7caa16bf9555e894836706a48da8ba2
Phil Hawker <philhawker1211@gmail.com>
2020-02-27 13:54:42 -0700

    converted class component into functional component. added export default to bottom of page

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default class NavigationContainer extends Component {
-    constructor() {
-        super()
-    }
+const NavigationComponent = (props) => {
 
-    render() {
-        return (
-            <div className='nav-wrapper'>
-                <div className='left-side'>
-                    <div className='nav-link-wrapper'>
-                        <NavLink exact to='/' activeClassName='nav-link-active'>
-                            Home
-                        </NavLink>
-                    </div>
 
-                    <div className='nav-link-wrapper'>
-                        <NavLink to='/about-me' activeClassName='nav-link-active'>
-                            About
-                        </NavLink>
-                    </div>
-
-                    <div className='nav-link-wrapper'>
-                        <NavLink to='/contact' activeClassName='nav-link-active'>
-                            Contact
-                        </NavLink>
-                    </div>
-
-                    <div className='nav-link-wrapper'>
-                        <NavLink to='/Blog' activeClassName='nav-link-active'>
-                            Blog
-                        </NavLink>
-                    </div>
+    return (
+        <div className='nav-wrapper'>
+            <div className='left-side'>
+                <div className='nav-link-wrapper'>
+                    <NavLink exact to='/' activeClassName='nav-link-active'>
+                        Home
+                    </NavLink>
                 </div>
 
-                <div className='right-side'>Phil Hawker</div>
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/about-me' activeClassName='nav-link-active'>
+                        About
+                    </NavLink>
+                </div>
+
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/contact' activeClassName='nav-link-active'>
+                        Contact
+                    </NavLink>
+                </div>
+
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/Blog' activeClassName='nav-link-active'>
+                        Blog
+                    </NavLink>
+                </div>
             </div>
+
+            <div className='right-side'>Phil Hawker</div>
+        </div>
         )
     }
-}
+
+export default NavigationComponent
