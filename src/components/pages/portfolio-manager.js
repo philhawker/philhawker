7f1c538a920fc770712584e731d49e46abe390d2
Phil Hawker <philhawker1211@gmail.com>
2020-02-28 12:40:28 -0700

    created api call function to get portfolio items from database and update state

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 // call api
 // bring portfolio items in
@@ -7,6 +8,29 @@ import React, {Component} from 'react'
 
 
 export default class PortfolioManager extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        axios.get('https://philhawker.devcamp.space/portfolio/portfolio_items', {withCredentials: true}
+        ).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            })
+        }).catch(error => {
+            console.log('error in getPortfolioItems', error)
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems()
+    }
+
     render () {
         return (
             <div className='portfolio-manager-wrapper'>
