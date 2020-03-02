db0a0c7e39c7e68f0f1171bc9fdc9be5c556a95d
Phil Hawker <philhawker1211@gmail.com>
2020-03-02 15:03:20 -0700

    changed description to text area tag

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -101,7 +101,7 @@ export default class PortfolioForm extends Component {
                     </div>
 
                     <div>
-                        <input
+                        <textarea
                             type="text"
                             name="description"
                             placeholder="Description"
