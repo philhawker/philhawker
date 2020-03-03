b06445ad5af407deeab14db574b03b2cfe30f974
Phil Hawker <philhawker1211@gmail.com>
2020-03-02 23:10:11 -0700

    added and styled icon for editing

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -9,12 +9,20 @@ export default class PortfolioManager extends Component {
         super()
 
         this.state = {
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {},
         }
 
         this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleEditClick(portfolioItem) {
+        this.setState({
+            portfolioToEdit: portfolioItem
+        })
     }
 
     handleDeleteClick(portfolioItem) {
@@ -71,6 +79,7 @@ export default class PortfolioManager extends Component {
                     <PortfolioSidebarList
                         handleDeleteClick={ this.handleDeleteClick }
                         data={ this.state.portfolioItems }
+                        handleEditClick={this.handleEditClick}
                     />
                 </div>
             </div>
