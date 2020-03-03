2f9b5a9d5e458ced0b837539bdb284f7d7a329f8
Phil Hawker <philhawker1211@gmail.com>
2020-03-03 16:24:55 -0700

    created default state of empty fields and functionality to populated fields with existing data when clicking edit icon

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -34,6 +34,33 @@ export default class PortfolioForm extends Component {
         this.logoRef = React.createRef();
     }
 
+    componentDidUpdate() {
+        if (Object.keys(this.props.portfolioToEdit).length > 0) {
+            const {
+                id,
+                name,
+                description,
+                category,
+                position,
+                url,
+                thumb_image_url,
+                banner_image_url,
+                logo_url
+            } = this.props.portfolioToEdit
+
+            this.props.clearPortfolioToEdit();
+
+            this.setState({
+                id: id,
+                name: name || '',    // '||' this is pipe characters. stands for 'OR'
+                description: description || '',
+                category: category || 'eCommerce',
+                position: position || '',
+                url: url || '',
+            })
+        }
+    }
+
     handleThumbDrop() {
         return {
             addedfile: file => this.setState({ thumb_image: file })
