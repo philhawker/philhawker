18031cc5619294c7413f369e52f6600d51ef7436
Phil Hawker <philhawker1211@gmail.com>
2020-03-02 22:31:26 -0700

    added sign out icon from fontawesome library. styled with pointer and hover effect

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { withRouter } from 'react-router' // higher order component. noticed it isnt capatalized
 import { NavLink } from 'react-router-dom'
 
@@ -59,7 +60,9 @@ const NavigationComponent = (props) => {
             <div className='right-side'>
             PHIL M. HAWKER
 
-            {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>Log Out</a> : null}
+            {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>
+                <FontAwesomeIcon icon='sign-out-alt' />
+            </a> : null}
             </div>
         </div>
         )
