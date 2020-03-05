8d4b4e00c591a73721b0fcf7a3a0a0c4729cac0d
Phil Hawker <philhawker1211@gmail.com>
2020-03-04 17:25:35 -0700

    created functionality to populated blog page with posts from databse API

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function () {
-    return (
-        <div>
-            <h2>Blog</h2>
+class Blog extends Component {
+    constructor(props) {
+        super(props);
 
-            <div>
-                <Link to='/about-me'>Find out more about myself></Link>
-            </div>
-        </div>
-    )
+        this.state = {
+            blogItems: []
+        };
+
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+    getBlogItems() {
+        axios.get('https://philhawker.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true }
+        ).then(response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        }).catch(error => {
+            console.log('getBlogItem error', error);
+        });
+    }
+
+    componentWillMount() {
+        this.getBlogItems();
+    }
+
+    render() {
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <h1>{ blogItem.title }</h1>
+        })
+
+        return <div>{ blogRecords }</div>
+    }
 }
+
+export default Blog;
