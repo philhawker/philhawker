92c1cdb72bcb469fcb7ed90f8a033a44fd565098
Phil Hawker <philhawker1211@gmail.com>
2020-03-04 18:56:06 -0700

    implemented styling for blog items on page

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -34,11 +34,17 @@ export default class BlogDetail extends Component {
             blog_status,
         } = this.state.blogItem
         return (
-            <div>
-                <h1>{ title }</h1>
-                <img src={ featured_image_url } />
-                <div>
-                    { content }
+            <div className='blog-container'>
+                <div className='content-container'>
+                    <h1>{ title }</h1>
+
+                    <div className='featured-image-wrapper'>
+                        <img src={ featured_image_url } />
+                    </div>
+
+                    <div className='content'>
+                        { content }
+                    </div>
                 </div>
             </div>
         )
