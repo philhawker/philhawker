9eaec606f0afd0519c2c4ec53f0966f5497cc1d7
Phil Hawker <philhawker1211@gmail.com>
2020-03-05 18:32:26 -0700

    finalized infinite scrolling feature and applied loading rules

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,8 +22,13 @@ class Blog extends Component {
 
     activateInfiniteScroll() {
         window.onscroll = () => {
+            if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+                return
+            }
+
+
             if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-                console.log('get more posts')
+                this.getBlogItems()
             }
         }
     }
@@ -33,10 +38,11 @@ class Blog extends Component {
             currentPage: this.state.currentPage + 1
         })
 
-        axios.get('https://philhawker.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true }
+        axios.get(`https://philhawker.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, { withCredentials: true }
         ).then(response => {
+            console.log('getting', response.data)
             this.setState({
-                blogItems: response.data.portfolio_blogs,
+                blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
                 totalCount: response.data.meta.total_records,
                 isLoading: false,
             })
