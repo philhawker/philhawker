4414ae3e8b25af4441bc1ef738c8d45d793d78f1
Phil Hawker <philhawker1211@gmail.com>
2020-03-05 17:31:32 -0700

    created currentPage incrementer to know what page the scroll is on

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ class Blog extends Component {
         super(props);
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0,
         };
 
 
@@ -25,10 +27,15 @@ class Blog extends Component {
     }
 
     getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
+
         axios.get('https://philhawker.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true }
         ).then(response => {
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: response.data.portfolio_blogs,
+                totalCount: response.data.meta.total_records,
             })
         }).catch(error => {
             console.log('getBlogItem error', error);
