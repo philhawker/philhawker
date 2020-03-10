c21e66edda7370063e4c5ac318c422d22efa1cb5
Phil Hawker <philhawker1211@gmail.com>
2020-03-10 13:27:08 -0600

    styled modal size and background color

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -4,13 +4,30 @@ import ReactModal from 'react-modal'
 export default class BlogModal extends Component {
     constructor(props) {
         super(props)
+
+
+        this.customStyles = {    //this "in-line styling" needs to be used with the modal library in react
+            content: {
+                top: '50%',
+                left: '50%',
+                right: 'auto',
+                marginRight: '-50%',
+                transform: 'translate(-50%, -50%)',
+                width: '800px',
+            },
+            overlay: {
+                backgroundColor: 'rgba(1, 1, 1, 0.75)'
+            }
+        }
     }
 
     render() {
         return (
-            <ReactModal onRequestClose={() => {
-                this.props.handleModalClose()
-            }} isOpen={this.props.modalIsOpen}>
+            <ReactModal
+                style={this.customStyles}
+                onRequestClose={() => {
+                    this.props.handleModalClose()
+                }} isOpen={this.props.modalIsOpen}>
                 <h1>in a modal</h1>
             </ReactModal>
         )
