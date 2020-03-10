67cea653fb990caed893d98b0434f0f7eca0b183
Phil Hawker <philhawker1211@gmail.com>
2020-03-10 14:14:23 -0600

    connected blog api with modal functions so that databse is updated from modal submission

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -15,8 +16,23 @@ export default class BlogForm extends Component {
     }
 
 
+    buildForm() {
+        let formData = new FormData()
+
+        formData.append('portfolio_blog[title]', this.state.title)
+        formData.append('portfolio_blog[blog_status]', this.state.blog_status)
+
+        return formData
+    }
+
     handleSubmit(event) {
-        this.props.handleSuccessfulFormSubmission(this.state)
+        axios.post('https://philhawker.devcamp.space/portfolio/portfolio_blogs', this.buildForm(), { withCredentials: true }
+        ).then(response => {
+            this.props.handleSuccessfulFormSubmission(response.data)
+        }).catch(error => {
+            console.log('handleSubmit for blog error', error)
+        })
+
         event.preventDefault()
     }
 
