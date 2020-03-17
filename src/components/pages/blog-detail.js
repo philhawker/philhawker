3a9c6525f1c70833f477176e096995820748741a
Phil Hawker <philhawker1211@gmail.com>
2020-03-16 22:11:48 -0600

    added delete featured blog image via api and updating parent component state

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -16,6 +16,15 @@ export default class BlogDetail extends Component {
         }
 
         this.handleEditClick = this.handleEditClick.bind(this)
+        this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this)
+    }
+
+    handleFeaturedImageDelete() {
+        this.setState({
+            blogItem: {
+                featured_image_url: ''
+            }
+        })
     }
 
     handleEditClick() {
@@ -48,7 +57,13 @@ export default class BlogDetail extends Component {
 
         const contentManager = () => {
             if (this.state.editMode) {
-                return <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />
+                return (
+                    <BlogForm
+                        handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+                        editMode={this.state.editMode}
+                        blog={this.state.blogItem}
+                    />
+                )
             } else {
                 return (
                     <div className='content-container'>
