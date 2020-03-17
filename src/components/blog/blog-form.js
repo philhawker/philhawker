4ca48923043e34696e59e0f0d61ec6d7e9e52744
Phil Hawker <philhawker1211@gmail.com>
2020-03-16 22:32:40 -0600

    now communicates with blog update api endpoint

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -15,6 +15,8 @@ export default class BlogForm extends Component {
             blog_status: '',
             content: '',
             featured_image: '',
+            apiUrl: 'https://philhawker.devcamp.space/portfolio/portfolio_blogs',
+            apiAction: 'post',
         }
 
         this.handleChange = this.handleChange.bind(this)
@@ -48,7 +50,10 @@ export default class BlogForm extends Component {
             this.setState({
                 id: this.props.blog.id,
                 title: this.props.blog.title,
-                status: this.props.blog.status,
+                blog_status: this.props.blog.blog_status,
+                content: this.props.blog.content,
+                apiUrl: `https://philhawker.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+                apiAction: 'patch',
             })
         }
     }
@@ -94,8 +99,12 @@ export default class BlogForm extends Component {
     }
 
     handleSubmit(event) {
-        axios.post('https://philhawker.devcamp.space/portfolio/portfolio_blogs', this.buildForm(), { withCredentials: true }
-        ).then(response => {
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+        }).then(response => {
             if (this.state.featured_image) {
                 this.featuredImageRef.current.dropzone.removeAllFiles()
             }
@@ -107,8 +116,11 @@ export default class BlogForm extends Component {
                 featured_image: ''
             })
 
-
-            this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog)
+            if (this.props.editMode) {
+                this.props.handleUpdateFormSubmission(response.data.portfolio_blog)
+            } else {
+                this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog)
+            }
 
         }).catch(error => {
             console.log('handleSubmit for blog error', error)
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -17,6 +17,14 @@ export default class BlogDetail extends Component {
 
         this.handleEditClick = this.handleEditClick.bind(this)
         this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this)
+        this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(this)
+    }
+
+    handleUpdateFormSubmission(blog) {
+        this.setState({
+            blogItem: blog,
+            editMode: false,
+        })
     }
 
     handleFeaturedImageDelete() {
@@ -60,6 +68,7 @@ export default class BlogDetail extends Component {
                 return (
                     <BlogForm
                         handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+                        handleUpdateFormSubmission={this.handleUpdateFormSubmission}
                         editMode={this.state.editMode}
                         blog={this.state.blogItem}
                     />
