b68cf3481c43954fb7436c91b8a7f9edc26412e7
Phil Hawker <philhawker1211@gmail.com>
2020-03-17 00:15:58 -0600

    rendered portfolio detail data from api on screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 export default class PortfolioDetail extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            portfolioItem: {}
+        }
     }
 
     componentWillMount() {
@@ -13,16 +17,29 @@ export default class PortfolioDetail extends Component {
     getPortfolioItem() {
         axios.get(`https://philhawker.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true }
         ).then(response => {
-            console.log('res', response)
+            this.setState({
+                portfolioItem: response.data.portfolio_item,
+            })
         }).catch(error => {
             console.log("getportfolioitem error", error)
         })
     }
 
     render() {
+        const {
+            banner_image_url,
+            category,
+            description,
+            logo_url,
+            name,
+            thumb_image_url,
+            url,
+        } = this.state.portfolioItem
+
         return (
             <div>
-                <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+                <h2>{name}</h2>
+                <p>{description}</p>
             </div>
         )
     }
