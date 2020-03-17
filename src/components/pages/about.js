f719b1b95f9ad2589c0daa649926fa5a6746a4db
Phil Hawker <philhawker1211@gmail.com>
2020-03-16 23:26:44 -0600

    styled about me page and added profile pic

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import profilePicture from '../../../static/assets/images/bio/handholding.jpg'
 
 export default function () {
     return (
-        <div>
-            About
+        <div className='content-page-wrapper'>
+            <div
+                className='left-column'
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            />
+            <div className='right-column'>
+                Consectetur quasi odit voluptatibus pariatur quo quibusdam quam consequatur nisi facere cum numquam doloribus ipsam iure assumenda, iste delectus sit quis, at neque repellat eaque? Nemo cumque reiciendis magnam consequuntur. Modi neque odio velit deserunt eos. Facere dolorum magnam, quaerat est quos animi fugiat deserunt consequatur obcaecati tenetur molestiae fuga voluptates aliquid? Ipsum, molestias non. Cupiditate voluptatem in temporibus dolorem. Quia vitae a facilis doloremque dolores iusto, optio reiciendis recusandae in? Voluptatibus atque nihil rerum, molestias velit incidunt eligendi libero suscipit tempore eos consequatur commodi, magni sunt placeat nam nesciunt. Error culpa maxime quas repellendus ipsa, saepe reprehenderit maiores voluptate cupiditate laudantium voluptatem odio repudiandae modi in blanditiis hic tenetur sed quod perspiciatis odit voluptates impedit! Nulla nemo ex eveniet.
+        </div>
         </div>
     )
 }
